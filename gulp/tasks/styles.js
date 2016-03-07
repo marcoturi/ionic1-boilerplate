eb821878613724203d5c6f17220a0d73a5a7ab15
Marco Turi <marco.turi@hotmail.it>
2016-03-07 16:12:06 +0100

    build task

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -1,8 +1,3 @@
-/**
- * @author    Martin Micunda {@link http://martinmicunda.com}
- * @copyright Copyright (c) 2015, Martin Micunda
- * @license   GPL-3.0
- */
 'use strict';
 
 import gulp from 'gulp';
