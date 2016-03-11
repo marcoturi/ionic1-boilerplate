e3068d8aa09f8803f46a02a2a7290655a1f60ddc
Marco Turi <marco.turi@hotmail.it>
2016-03-11 10:23:35 +0100

    gulp serve and build

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -10,14 +10,17 @@ import path from '../paths';
  */
 gulp.task('watch', () => {
     // Watch images and fonts files
-    gulp.watch([path.app.images, path.app.fonts], [browserSync.reload]);
+    gulp.watch([path.app.images, path.app.fonts], ['build'], [browserSync.reload]);
 
     // Watch css files
-    gulp.watch(path.app.styles, ['sass']);
+    gulp.watch(path.app.styles, ['build'], [browserSync.reload]);
 
     // Watch js files
-    gulp.watch(path.app.scripts,  browserSync.reload);
+    gulp.watch(path.app.scripts,  ['build'], [browserSync.reload]);
+
+    // Watch tpl files
+    gulp.watch(path.app.templates,  ['build'], [browserSync.reload]);
 
     // Watch html files
-    gulp.watch([path.app.html, path.app.templates],  browserSync.reload);
+    gulp.watch(path.app.html, ['build'], [browserSync.reload]);
 });
